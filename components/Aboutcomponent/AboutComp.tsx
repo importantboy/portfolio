153dc403a9ffd484621a02cc9698b072afafb6b1
importantboy <importantinbox123@gmail.com>
2025-07-28 20:10:25 +0530

    larger changes

--- components/Aboutcomponent/AboutComp.tsx
+++ components/Aboutcomponent/AboutComp.tsx
@@ -6,7 +6,7 @@ export default function AboutComp() {
       <h2 className="text-3xl md:text-6xl text-gray-600 dark:text-white py-6 text-center">
         About me
       </h2>
-      <CardSpotlight>
+      <CardSpotlight color="">
         <div
           className="w-3/4 m-auto rounded-lg bg-black border-1 border-gray-600 h-[70vh]
       text-gray-600 dark:text-gray-50 shadow-blue-50 p-5 "
